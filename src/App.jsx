ea2f332461825eda2b4794691a38a47b77e21f70
John Hunter <johnroberthunter1@gmail.com>
2026-03-30 07:56:48 -0600

    Initial commit from clean template

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import { MdFace3 } from "react-icons/md";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          Hello World <MdFace3 />
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
